e5bf2183611b97d1d1183517cf09485f5332836c
Ben Mills <fuzzyllama@gmail.com>
2021-10-26 13:25:54 -0700

    Finishes feature 1 and feature 2

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 This repo is a simple project using Reactjs. The project is a series of assignments from the first week of Code 301 at Seattle Code Fellows.
 
+## Class 01
+
+Set up Header, Footer, Main and HornedBeasts react components displaying 3 horned beasts in the main.
+
+## Class 02
+
+### Feature 1 Display Images
+
+"As a user, I want to view the images on the page so that I can browse the photo collection."
+
+Number and name of feature: #1 Display Images
+
+- Estimate of time needed to complete: 20 minutes
+- Start time: 12:57PM
+- Finish time: 
+- Actual time needed to complete: 
+
--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,11 +1,20 @@
 import {Component} from "react";
 
 class HornedBeasts extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      numClicks: 0,
+    }
+  }
+
   render() {
     return (
       <div id="beast">
         <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
+        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} onClick={() => this.setState({numClicks: this.state.numClicks + 1})} />
+        <p>❤️{this.state.numClicks}</p>
         <p>{this.props.description}</p>
       </div>
     )
